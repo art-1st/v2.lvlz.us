bb56466973d9065dd9be4344a206ee5d7ffe30b1
YoonHwan Cho <leo@devunlimit.com>
2020-08-20 23:58:46 +0900

    implement firebase authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,37 @@
 import { AppProps } from 'next/app';
 import { Provider } from 'mobx-react';
+import firebase from 'firebase/app';
 import RootStore from '~/stores';
 import { ThemeProvider } from 'styled-components';
 import theme from '~/theme';
 
+import 'firebase/auth';
 import 'mobx-react/batchingForReactDom';
 import 'tui-calendar/dist/tui-calendar.css';
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
+import { useEffect } from 'react';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+      firebase.auth().onAuthStateChanged(user => {
+        console.log(user);
+      });
+    }
+  });
+
   return (
     <Provider {...RootStore}>
       <ThemeProvider theme={theme}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import Link from 'next/link';
+import firebase from 'firebase/app';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 
+import 'firebase/auth';
+
 const Home = () => {
+  useEffect(() => {
+    console.log('home');
+  }, []);
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        console.log(result);
+      });
+  };
+
   return (
     <Container>
       <h1>Index</h1>
@@ -9,6 +27,8 @@ const Home = () => {
         <Link href="/calendar">
           <a>Calendar</a>
         </Link>
+        <hr />
+        <button onClick={() => login()}>Login</button>
       </div>
     </Container>
   );
