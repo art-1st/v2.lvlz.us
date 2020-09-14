1d67dc4d7a1c374b6c973031994fb65c43ecea34
YoonHwan Cho <leo@devunlimit.com>
2020-09-14 23:59:01 +0900

    implement schedules to fullcalendar

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,21 +1,46 @@
 import { NextPage } from 'next';
+import { useRef, useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import styled from 'styled-components';
+import { getSchedule, IScheduleData } from '~/database/schedule';
 
-interface Props {
-  // scheduleData: IScheduleData[];
-}
+interface Props {}
 
 const CalendarPage: NextPage<Props> = () => {
+  const fcRef = useRef<FullCalendar>(null);
+  const [schedules, setSchedules] = useState<IScheduleData[]>([]);
+
+  useEffect(() => {
+    const API = fcRef.current!.getApi();
+    const startAt = API.view.activeStart.toISOString();
+    const endAt = API.view.activeEnd.toISOString();
+
+    onChangeDateRange(startAt, endAt);
+
+    API.on('datesSet', d => {
+      onChangeDateRange(d.start.toISOString(), d.end.toISOString());
+    });
+  }, []);
+
+  const onChangeDateRange = async (startAt: string, endAt: string) => {
+    const schedules = await getSchedule(startAt, endAt);
+
+    setSchedules(prevSchedules => {
+      return [...prevSchedules, ...schedules];
+    });
+  };
+
   return (
     <CalendarContainer>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
-      ></FullCalendar>
+        ref={fcRef}
+        events={schedules as any}
+      />
     </CalendarContainer>
   );
 };
